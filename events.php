4781d8dd30050f8bcaff6d56f8d017cf07e6dd96
bhaumik93 <bhaumik93@gmail.com>
2013-03-02 23:29:23 +0530

    events contact

--- events.php
+++ events.php
@@ -24,8 +24,8 @@
 		<br><br><br>There’s a whole world out there to understand, but nothing is going to come easy. Anyone with confidence in their grey cells is welcome to the challenge of Puzzle Champ. This event keeps away from typical text book quizzing and scientific queries, dealing with a broader range of inquiry, enticing participants and engrossing them with puzzling puzzles.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
+
 	</center>
 </div>
 
@@ -34,8 +34,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 		<br><br><br>Creating a computer simulated model opens-up a lot of possibilities to experiment and examine the characteristics of a structure, than its model or prototype. Desmod incorporates the ability of participants to metamorphose their imagination to reality with a sound knowledge of design. The event will have a technical introduction session to new software, followed by a hands-on workshop.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328.
+<b>Contact details</b><br>
+Shashanka:9498053235, Prudvi Saroj: 8754437327, Netu:9003223364.
 	</center>
 </div>
 
@@ -44,8 +44,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328.
 		<br><br><br>The Wavez Triathlon involves a series of three challenges where the participants’ practical engineering skills are put to test, solving problems faced by the industry. The event tests the knowledge of the participants on various aspects of ocean engineering, on their managerial aptitude as well as their ability to work in a team. Being multi-disciplinary, it promises to be the perfect platform for the multi-faceted engineers.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details of </b><br>
+D V Rakesh: 9500101516 ,Srikant: 9884682331, Suresh Kumar:9791164845
 	</center>
 </div>
 
@@ -54,8 +54,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 		<br><br><br>How Structures Fail tests the reasoning and rational construction of scientific explanations in answering some of most bizarre phenomenon that led to the destruction of structures in catastrophes and other occurrences. While this makes it sound perplexing and beyond the scope of a normal curious mind, all that the event tests is a few fundamental scientific concepts. One always goes back richer, intellectually.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
+Pranav : 9489577068 Anivaran :9962609617 Raviteja:9884299766
 	</center>
 </div>
 
@@ -64,8 +64,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 		<br><br><br>Being one of the premier institutes in the field of engineering and technology, Lectures in Wavez are the locus of attractions for a large number of students, faculty and research associates. Lectures at Wavez have always been about innovators and top brass from the field of Naval Architecture and Ocean Engineering.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
+
 	</center>
 </div>
 
@@ -74,8 +74,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 		<br><br><br>The Department of Ocean Engineering has always witnessed advancement and innovations in ocean technology, and Open House serves as a medium to exhibit these scientific developments to enthusiastic school and college students and various industry professionals. Open house gives a glimpse into the ongoing projects and the world class research facilities at the department.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
+Mohit :9600089445 Atul:8939106690 Vani:9498073514 Amulya:9043812203
 	</center>
 </div>
 
@@ -84,8 +84,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 		<br><br><br>Another exciting, hands-on and outdoor event that ignites memories of adventure, Treasure Hunt is a picture of fun and camaraderie, giving the visitors plenty to brood over and laugh about at the same time. Participants come in teams, and the hunt stretches from morn to noon, noon to night.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
+Shanmukh :9962670804 Apeksha:9043818803 Nikhil:9962250677
 	</center>
 </div>
 
@@ -94,8 +94,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 		<br><br><br>Gamedrome is an event for all the gamers out there; professionals, amateurs and part-timers alike. With a line-up of games that includes crowd favourites like Counter Strike, FIFA and Need for Speed, Gamedrome is set to scorch the fest-turf with a horde of participants going neck-to-neck to come on top. Always the most exciting and enjoyable event, the promise of fun, adrenaline and cash prizes makes it a huge attraction.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
+Vishal:9962670955 Vignesh:9444895414 Vamsi:9884685196
 	</center>
 </div>
 
@@ -104,8 +104,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 		<br><br><br>The wavez main quiz is the flagship quizzing event of Wavez. Over the years, it has acquired a well-deserved reputation of hosting a highly competitive event while providing an exciting battle platform to the quizzing enthusiasts in IIT Madras. The Department Seminar Hall turns into a fierce arena during the prelims and finals of the event, attracting not just participants but an involved audience as well.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
+Pranav : 9489577068 Anivaran :9962609617 Raviteja:9884299766
 	</center>
 </div>
 
@@ -114,8 +114,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 		<br><br><br>Project X is a challenging event which tests the creativity and resourcefulness of the participants. The event is about solving innovative problems using basic everyday materials. This is a test of the enthusiasm and craftsmanship of the participants; a promised crowd-puller.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
+Chaitanya :9498073495 Revath:9677165465
 	</center>
 </div>
 
@@ -126,8 +126,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 <?php include"home/rexpo/uploadtext.php"; ?>
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
+Vardharajan :94456754997 Kavya :8148726481 Sakshi :949809052
 	</center>
 </div>
 
@@ -136,8 +136,8 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 		<br><br><br>Daily Events is a collection of puzzles, crosswords and other brain teasers that keep the attendees engrossed throughout their presence at the fest, covering the two days that Wavez is conducted. Everyone at the fest is seen racking their brains over these little pieces of intriguing puzzles, having their bit of enjoyment. This event would see the maximum participation, providing a great avenue for publicity for the sponsors and their brands.
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
+Ambedkar :8124223163
 	</center>
 </div>
 
@@ -146,9 +146,9 @@ Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
 		<br><br><br>Commemorating the success of Roboceana in Wavez ‘12, we are back with a brand new audience, deadlier problems and an ultimate showdown. Continuing to be the most anticipated event of Wavez, Roboceana promises loads of fun and excitement as the participants battle out to establish their skills with circuits and motors, and the one with the magic touch takes it all.<br><br><b><a href="files/Roboceana.pdf" target="_blank">Click here to view Roboceana problem statement. </a></b>
 
 <br><br>
-<b>Contact details of core team</b><br>
-Sai Nithin Krishna: 9884311892 and Srinija: 8148839328
+<b>Contact details </b><br>
 
+Sanchit :9003208651 Mukesh :8056076294 Srinadh :843980903
 	</center>
 </div>
 
